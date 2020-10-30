abba1d7dcef2e4a770d7245d3377f727f33cebd7
Henri Bergius <henri.bergius@iki.fi>
2020-10-30 13:25:07 +0100

    We can use classes now

--- index.js
+++ index.js
@@ -9,117 +9,119 @@ Object.defineProperty(exports, 'PING_INTERVAL', {
   enumerable: true,
 });
 
-exports.Runtime = function (runtime, options) {
-  if (typeof runtime !== 'object') {
-    throw new Error('Runtime options expected');
-  }
-  if (!runtime.id) {
-    throw new Error('Runtime requires an UUID');
-  }
+class Runtime {
+  constructor(runtime, options) {
+    if (typeof runtime !== 'object') {
+      throw new Error('Runtime options expected');
+    }
+    if (!runtime.id) {
+      throw new Error('Runtime requires an UUID');
+    }
 
-  this.runtime = runtime;
-  this.options = {};
-  Object.keys(defaults).forEach((name) => {
-    this.options[name] = defaults[name];
-  });
-
-  if (options) {
-    Object.keys(options).forEach((name) => {
-      this.options[name] = options[name];
+    this.runtime = runtime;
+    this.options = {};
+    Object.keys(defaults).forEach((name) => {
+      this.options[name] = defaults[name];
     });
-  }
-};
 
-exports.Runtime.prototype.register = function (t, c) {
-  let token = t;
-  let callback = c;
-  if (typeof token === 'function') {
-    callback = t;
-    token = null;
+    if (options) {
+      Object.keys(options).forEach((name) => {
+        this.options[name] = options[name];
+      });
+    }
   }
-  if (!this.runtime.user) {
-    callback(new Error('Runtime registration requires a user UUID'));
-    return;
-  }
-  if (!this.runtime.address) {
-    callback(new Error('Runtime registration requires an address URL'));
-    return;
-  }
-  if (!this.runtime.protocol) {
-    callback(new Error('Runtime registration requires a protocol'));
-    return;
-  }
-  if (!this.runtime.type) {
-    callback(new Error('Runtime registration requires a type'));
-    return;
-  }
-  if (token) {
+
+  register(t, c) {
+    let token = t;
+    let callback = c;
+    if (typeof token === 'function') {
+      callback = t;
+      token = null;
+    }
+    if (!this.runtime.user) {
+      callback(new Error('Runtime registration requires a user UUID'));
+      return;
+    }
+    if (!this.runtime.address) {
+      callback(new Error('Runtime registration requires an address URL'));
+      return;
+    }
+    if (!this.runtime.protocol) {
+      callback(new Error('Runtime registration requires a protocol'));
+      return;
+    }
+    if (!this.runtime.type) {
+      callback(new Error('Runtime registration requires a type'));
+      return;
+    }
+    if (token) {
+      superagent.put(`${this.options.host}/runtimes/${this.runtime.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(this.runtime)
+        .end(callback);
+      return;
+    }
     superagent.put(`${this.options.host}/runtimes/${this.runtime.id}`)
-      .set('Authorization', `Bearer ${token}`)
       .send(this.runtime)
       .end(callback);
-    return;
   }
-  superagent.put(`${this.options.host}/runtimes/${this.runtime.id}`)
-    .send(this.runtime)
-    .end(callback);
-};
 
-exports.Runtime.prototype.ping = function (callback) {
-  superagent.post(`${this.options.host}/runtimes/${this.runtime.id}`)
-    .send({})
-    .end((err) => {
-      if (callback) {
-        callback(err);
-      }
-    });
-};
-
-exports.Runtime.prototype.get = function (token, callback) {
-  if (!token) {
-    callback(new Error('API token required for fetching'));
-    return;
+  ping(callback) {
+    superagent.post(`${this.options.host}/runtimes/${this.runtime.id}`)
+      .send({})
+      .end((err) => {
+        if (callback) {
+          callback(err);
+        }
+      });
   }
-  superagent.get(`${this.options.host}/runtimes/${this.runtime.id}`)
-    .set('Authorization', `Bearer ${token}`)
-    .end((err, res) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      if (res.status !== 200) {
-        callback(new Error(`Request returned ${res.status}`));
-        return;
-      }
-      Object.keys(res.body).forEach((name) => {
-        if (name === 'seen' || name === 'registered') {
-          this.runtime[name] = new Date(res.body[name]);
+
+  get(token, callback) {
+    if (!token) {
+      callback(new Error('API token required for fetching'));
+      return;
+    }
+    superagent.get(`${this.options.host}/runtimes/${this.runtime.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        if (err) {
+          callback(err);
           return;
         }
-        this.runtime[name] = res.body[name];
+        if (res.status !== 200) {
+          callback(new Error(`Request returned ${res.status}`));
+          return;
+        }
+        Object.keys(res.body).forEach((name) => {
+          if (name === 'seen' || name === 'registered') {
+            this.runtime[name] = new Date(res.body[name]);
+            return;
+          }
+          this.runtime[name] = res.body[name];
+        });
+        callback(null, this.runtime);
       });
-      callback(null, this.runtime);
-    });
-};
-
-exports.Runtime.prototype.del = function (token, callback) {
-  if (!token) {
-    callback(new Error('API token required for deletion'));
-    return;
   }
-  superagent.del(`${this.options.host}/runtimes/${this.runtime.id}`)
-    .set('Authorization', `Bearer ${token}`)
-    .end((err, res) => {
-      if (err && err.status === 404) {
-      // If the runtime is already gone, that is fine too
-        callback();
-        return;
-      }
-      callback(err, res);
-    });
-};
 
-exports.list = function (token, o, c) {
+  del(token, callback) {
+    if (!token) {
+      callback(new Error('API token required for deletion'));
+      return;
+    }
+    superagent.del(`${this.options.host}/runtimes/${this.runtime.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        if (err && err.status === 404) {
+          // If the runtime is already gone, that is fine too
+          callback();
+          return;
+        }
+        callback(err, res);
+      });
+  }
+}
+
+function list(token, o, c) {
   let options = o;
   let callback = c;
   if (!callback) {
@@ -159,4 +161,7 @@ exports.list = function (token, o, c) {
       });
       callback(null, results);
     });
-};
+}
+
+module.exports.Runtime = Runtime;
+module.exports.list = list;
